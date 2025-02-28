8549e1e4386b0983142e2ca1c7fd97baf073a862
itzSushil <sushilsharma8oct2001@gmail.com>
2025-03-01 02:07:07 +0530

    .

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -23,7 +23,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-0$nv*r_sgf7qz)a94kd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
